7389f92ec28c876afb6fe74ad1d188883267edd7
Kai Morwinski <morckman@gmail.com>
2016-10-09 02:09:08 +0200

    Port main() to Qt5 and KF5
    The deprecated classes K4AboutData, KApplication and KCmdLineArgs have
    been ported to KAboutData, QApplication and QCommandLineParser.

--- src/main.cpp
+++ src/main.cpp
@@ -18,30 +18,54 @@
 *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
 ***************************************************************************/
 
-#include <KApplication>
-#include <K4AboutData>
-#include <KCmdLineArgs>
+#include <KAboutData>
+#include <KLocalizedString>
+
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QDir>
 
 #include "cirkuitconfig.h"
 #include "mainwindow.h"
 
 int main (int argc, char *argv[])
 {
-    K4AboutData aboutData( "cirkuit", "cirkuit", ki18n("Cirkuit"), VERSION, ki18n("An application to generate publication-ready figures. It is a KDE frontend for Circuit Macros by J. D. Aplevich, TikZ and Gnuplot. <p>Visit the <a href=http://www.ece.uwaterloo.ca/~aplevich/Circuit_macros>Circuit Macros</a> and <a href=http://www.texample.net/tikz>TikZ</a> websites for further information."), K4AboutData::License_GPL, ki18n("(c) 2011 Matteo Agostinelli"));
-    aboutData.addAuthor(ki18n("Matteo Agostinelli"), ki18n("Maintainer"), "matteo@agostinelli.me", "http://agostinelli.me");
-    aboutData.setHomepage("http://projects.kde.org/cirkuit");
-    KCmdLineArgs::init( argc, argv, &aboutData );
+    QApplication app(argc, argv);
 
-    KCmdLineOptions options;
-    options.add("+[file]", ki18n("Document to open"));
-    KCmdLineArgs::addCmdLineOptions( options );
+    KLocalizedString::setApplicationDomain("cirkuit"); // set translation catalogue name for KAboutData
 
-    KApplication app;
+    KAboutData aboutData(QStringLiteral("cirkuit"),
+                         i18n("Cirkuit"),
+                         QStringLiteral(VERSION),
+                         i18n("An application to generate publication-ready figures. "
+                              "It is a KDE frontend for Circuit Macros by J. D. Aplevich, TikZ and Gnuplot."),
+                         KAboutLicense::GPL,
+                         i18n("(c) 2011 Matteo Agostinelli"),
+                         i18n("Visit the <a href=http://www.ece.uwaterloo.ca/~aplevich/Circuit_macros>Circuit Macros</a> "
+                              "and <a href=http://www.texample.net/tikz>TikZ</a> websites for further information."),
+                         QStringLiteral("http://projects.kde.org/cirkuit"));
+
+    aboutData.addAuthor(i18n("Matteo Agostinelli"),
+                        i18n("Maintainer"),
+                        QStringLiteral("matteo@agostinelli.me"),
+                        QStringLiteral("http://agostinelli.me"));
+
+    KAboutData::setApplicationData(aboutData);
+
+    QCommandLineParser parser;
+    parser.addPositionalArgument(QStringLiteral("+[File]"), i18n("Document to open"));
+    aboutData.setupCommandLine(&parser); // add help, version, license, author option and set all infos about cirkuit
+
+    parser.process(app);
+    aboutData.processCommandLine(&parser); // evaluate author and license option
+
     MainWindow* window = new MainWindow();
 
-    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
-    if (args->count()) {
-        window->loadFile(args->url(0).url());
+    const QStringList args = parser.positionalArguments();
+    if (args.count()) {
+        // allow relative files (even non existing)
+        QUrl url = QUrl::fromUserInput(args.at(0), QDir::currentPath(), QUrl::AssumeLocalFile);
+        window->loadFile(url);
     }
 
     window->show();
